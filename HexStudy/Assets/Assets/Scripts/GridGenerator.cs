b55452f32e4da61fa0edf207ccb0fb46f5823a5e
GitWillG <willgrant0311@gmail.com>
2023-08-18 13:46:10 -0400

    Core functionality
    basic grid generation for an X-by Y grid size

--- HexStudy/Assets/Assets/Scripts/GridGenerator.cs
+++ HexStudy/Assets/Assets/Scripts/GridGenerator.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class GridGenerator : MonoBehaviour
 {
-    //nnumber of tiles along x axis of grid
+    //number of tiles along x axis of grid
     public int Width;
     //number of tiles along z axis of grid
     public int Depth;
 
     public GameObject hexPrefab;
+    public GameObject[,] hexArray;
 
 
     //actual dimensions of our prefab for refference
@@ -21,15 +22,45 @@ public class GridGenerator : MonoBehaviour
     //xOffset is half of the Width
     public float xOffset = 0.866f;
 
-    // Start is called before the first frame update
-    void Start()
+
+    /// <summary>
+    /// Exposing below to inspector
+    /// </summary>
+    [ContextMenu("Generate Grid")]
+    public void DefaultGeneration()
     {
-        
+        GridGeneration(Width, Depth);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Generates a gWidth x gDepth grid of hexes
+    /// </summary>
+    /// <param name="gWidth">number of hexes along the x axis</param>
+    /// <param name="gDepth">number of hexes along the z axis</param>
+    public void GridGeneration(int gWidth, int gDepth)
     {
-        
+        Width = gWidth;
+        Depth = gDepth;
+        hexArray = new GameObject[Width, Depth];
+        //2 Dimension grid
+        for (int x = 0; x < Width; x++)
+        {
+            for (int z = 0; z < Depth; z++)
+            {
+                //Adjust x positioning based on tile width
+                float xPos = x * hexWidth;
+
+                //every other row we offset the x position for the zig-zag positioning
+                if (z % 2 == 1)
+                {
+                    xPos += xOffset;
+                }
+                //make a hex at the location and name it with its 2D dimensions
+                GameObject HexOb = (GameObject)Instantiate(hexPrefab, new Vector3(xPos, 0, z * zOffset), Quaternion.Euler(0, 90, 0));
+                HexOb.name = "Hex " + x + " " + z;
+                HexOb.transform.SetParent(this.gameObject.transform);
+                hexArray[x, z] = HexOb.gameObject;
+            }
+        }
     }
 }
