7466dca39c9b4232b0c70e791ad0ebe5b9019795
GitWillG <willgrant0311@gmail.com>
2023-09-06 17:20:33 -0400

    camera improvements
    removed irrelevant case study methods
    created methods to contain update functionality
    corrected update -> fixedupdate

--- HexStudy/Assets/Assets/Scripts/CameraController.cs
+++ HexStudy/Assets/Assets/Scripts/CameraController.cs
@@ -27,6 +27,7 @@ public class CameraController : MonoBehaviour
     Transform backAnchor;
     Transform leftAnchor;
     public float moveAmount = 10f;
+    public float edgeSize = 5f;
 
 
     // Camera rotation speed
@@ -53,193 +54,87 @@ public class CameraController : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
-        rotateCamera();
+        RotateCamera();
 
-        //  WASD Camera steer
-        if (!isPanning || !isTracking)
+        GetCameraInputs();
+
+        ZoomCamera();
+    }
+
+    /// <summary>
+    /// Pans camera along x/z axis based on mouse position or WASD controls
+    /// </summary>
+    private void GetCameraInputs()
+    {
+        //Touching an edge of the screen will also pan the camera in that direction
+        //Forward
+        if ((Input.GetKey(KeyCode.W)) || Input.mousePosition.y > Screen.height - edgeSize)
         {
-
-            if ((Input.GetKey(KeyCode.W)))
-            {
-                // anchor.transform.position += cameraRight * Time.deltaTime;
-                cameraFollow = Vector3.MoveTowards(cameraFollow, forwardAnchor.position, 1);
-            }
-            if ((Input.GetKey(KeyCode.S)))
-            {
-
-                cameraFollow = Vector3.MoveTowards(cameraFollow, backAnchor.position, 1);
-            }
-            if ((Input.GetKey(KeyCode.A)))
-            {
-                cameraFollow = Vector3.MoveTowards(cameraFollow, leftAnchor.position, 1);
-            }
-            // left control is for debug tools
-            if (!Input.GetKey(KeyCode.LeftControl) && (Input.GetKey(KeyCode.D)))
-            {
-                cameraFollow = Vector3.MoveTowards(cameraFollow, rightAnchor.position, 1);
-            }
-
-            m_Camera.transform.position = cameraFollow;
-
-
-            // TODO - Prevent steering when mouse is outside the game screen in unity editor
-            #region Edge steering
-            // Edge steering
-
-            //Touching an edge of the screen will also pan the camera in that direction
-            float edgeSize = 3f;
-            if (Input.mousePosition.x < Screen.width && Input.mousePosition.y < Screen.height)
-            {
-                if (Input.mousePosition.x > Screen.width - edgeSize)
-                {
-                    cameraFollow.x += moveAmount * Time.deltaTime;
-                }
-            if (Input.mousePosition.x < edgeSize)
-            {
-                cameraFollow.x -= moveAmount * Time.deltaTime;
-            }
-            if (Input.mousePosition.y > Screen.height - edgeSize)
-            {
-                cameraFollow.z += moveAmount * Time.deltaTime;
-            }
-            if (Input.mousePosition.y < edgeSize)
-            {
-                cameraFollow.z -= moveAmount * Time.deltaTime;
-            }
-            //Once we have moved the camera target to the appropriate position we move the actual camera there
-
+            cameraFollow = Vector3.MoveTowards(cameraFollow, forwardAnchor.position, 1);
         }
-        #endregion
+        //Backward
+        if ((Input.GetKey(KeyCode.S)) || Input.mousePosition.y < edgeSize)
+        {
+            cameraFollow = Vector3.MoveTowards(cameraFollow, backAnchor.position, 1);
+        }
 
+        //Left
+        if ((Input.GetKey(KeyCode.A)) || Input.mousePosition.x < edgeSize)
+        {
+            cameraFollow = Vector3.MoveTowards(cameraFollow, leftAnchor.position, 1);
+        }
+        //Right
+        if ((Input.GetKey(KeyCode.D)) || Input.mousePosition.x > Screen.width - edgeSize)
+        {
+            cameraFollow = Vector3.MoveTowards(cameraFollow, rightAnchor.position, 1);
+        }
+        
+    }
 
-        // Uses camera's Field of view to simulate zoom
-        // TODO - Use actual transforms, as FOV distorts the view
-        #region Scroll Wheel Zoom
-        // Scroll wheel zoom
+    /// <summary>
+    /// Controls the FOW to simulate zooming in/out
+    /// </summary>
+    private void ZoomCamera()
+    {
+        m_Camera.transform.position = cameraFollow;
 
         //get the Scroll wheel input
         float scrollData;
-            scrollData = Input.GetAxis("Mouse ScrollWheel");
-            //Only zoom if you are actually scrolling 
-            if (scrollData != 0.0)
-            {
-                //zoom in the direction scrolled
-                currentFOV += Mathf.Sign(scrollData) * zoomSpeed;
-            }
-            //limit zoom in and outwards
-            currentFOV = Mathf.Clamp(currentFOV, 20, 80);
-            //move the camera in the clamped limits
-            m_Camera.fieldOfView = currentFOV;
-            #endregion
-        }
-
-
-        // PANNING LOGIC
-        if (isPanning)
+        scrollData = Input.GetAxis("Mouse ScrollWheel");
+        //Only zoom if you are actually scrolling 
+        if (scrollData != 0.0)
         {
-            if (objectToPan == null) return;
-
-            panPosition = new Vector3
-            (
-                objectToPan.transform.position.x - 5,
-                cameraFollow.y,
-                objectToPan.transform.position.z - 5
-            );
-            cameraFollow = Vector3.MoveTowards(cameraFollow, panPosition, Mathf.Infinity);
-
-            if (Vector3.Distance(cameraFollow, panPosition) <= 3)
-            {
-                StartCoroutine("resetPan", 1 / 4);
-            }
+            //zoom in the direction scrolled
+            currentFOV += Mathf.Sign(scrollData) * zoomSpeed;
         }
-
-        // TRACKING LOGIC
-        if (isTracking)
-        {
-
-            panPosition = new Vector3
-            (
-                objectToPan.transform.position.x - 5,
-                cameraFollow.y,
-                objectToPan.transform.position.z - 5
-            );
-            cameraFollow = Vector3.MoveTowards(cameraFollow, panPosition, Mathf.Infinity);
-        }
-
-
+        //limit zoom in and outwards
+        currentFOV = Mathf.Clamp(currentFOV, 20, 80);
+        //move the camera in the clamped limits
+        m_Camera.fieldOfView = currentFOV;
     }
 
-    /// <summary>
-    /// Updates the object reference for camera to pan. Actual panning logic is in cameraControl's update. This just changes the target reference.
-    /// </summary>
-    /// <param name="targetObject"></param>
-    public void panToObject(GameObject targetObject)
-    {
-        isPanning = true;
-        objectToPan = targetObject;
-    }
 
-    public IEnumerator panToObjectWithDelay(GameObject target, float seconds)
-    {
-        yield return new WaitForSeconds(seconds);
-        panToObject(target);
-    }
-
-    /// <summary>
-    /// Updates the object reference for camera to track. Actual tracking logic is in cameraControl's update. This just changes the target reference.
-    /// </summary>
-    /// <param name="targetObject"></param>
-    public void trackObject(GameObject targetObject)
-    {
-        isTracking = true;
-        objectToPan = targetObject;
-
-    }
-
-    /// <summary>
-    /// Nullifies the target reference for camera and stops camera tracking.
-    /// </summary>
-    public void finishTracking()
-    {
-        isTracking = false;
-        objectToPan = null;
-
-    }
 
     /// <summary>
     /// Rotates the camera to right (right arrow key) or to left (left arrow key)
     /// </summary>
-    void rotateCamera()
+    void RotateCamera()
     {
         Vector3 rotation = transform.eulerAngles;
 
         if (Input.GetKey(KeyCode.E))
         {
             rotation.y += rotationSpeed * Time.deltaTime;
-            Debug.Log("Right arrow key");
         }
         if (Input.GetKey(KeyCode.Q))
         {
             rotation.y -= rotationSpeed * Time.deltaTime;
-            Debug.Log("Left arrow key");
         }
         transform.eulerAngles = rotation;
     }
 
 
-    /// <summary>
-    /// Nullifies the panning target after the time specified.
-    /// </summary>
-    /// <param name="time"></param>
-    /// <returns></returns>
-    IEnumerator resetPan(float time)
-    {
-        yield return new WaitForSeconds(time);
-        isPanning = false;
-        objectToPan = null;
-
-    }
 }
